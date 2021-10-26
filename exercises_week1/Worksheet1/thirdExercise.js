22f917feb4143a92f1908ed58a0f52ab58e1384c
Idkirsch <s195483@student.dtu.dk>
2021-10-26 15:01:04 +0200

    add texture to triangle

--- exercises_week1/Worksheet1/thirdExercise.js
+++ exercises_week1/Worksheet1/thirdExercise.js
@@ -14,6 +14,31 @@ window.onload = function init(){
  	gl.useProgram(program);
 
  	var vertices = [ vec2(0.0, 0.5), vec2(-0.5, -0.5), vec2(0.5, -0.5) ];
+ 	var colors = [ vec3(1,0,0), vec3(0,1,0), vec3(0,0,1)]
+ 	
+// TEXTURE --------------
+ 	var tex = gl.createTexture();
+ 	gl.bindTexture(gl.TEXTURE_2D, tex);
+ 	var level = 0;
+ 	var width = 2;
+ 	var height = 1;
+ 	var data = new Uint8Array([
+ 	   255, 0, 0, 255,   // a red pixel
+ 	   0, 255, 0, 255,   // a green pixel
+ 	]);
+
+ 	gl.texImage2D(gl.TEXTURE_2D, level, gl.RGBA, width, height, 0, gl.RGBA, gl.UNSIGNED_BYTE, data);
+ 	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+
+ 	var someSamplerLoc = gl.getUniformLocation(program, "u_texture");
+ 	
+ 	var unit = 5;  // Pick some texture unit
+ 	gl.activeTexture(gl.TEXTURE0 + unit);
+ 	gl.bindTexture(gl.TEXTURE_2D, tex);
+ 	gl.uniform1i(someSamplerLoc, unit);
+
+// ------------------
+
  	var vBuffer = gl.createBuffer();
  	gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
  	gl.bufferData(gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW);
@@ -22,14 +47,13 @@ window.onload = function init(){
  	gl.enableVertexAttribArray(vPosition);
 
 
- 	var colors = [ vec3(1,0,0), vec3(0,1,0), vec3(0,0,1)]
 	var cBuffer = gl.createBuffer();
  	gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
  	gl.bufferData(gl.ARRAY_BUFFER, flatten(colors), gl.STATIC_DRAW);
- 	
   	var vColor = gl.getAttribLocation(program, "a_Color");
  	gl.vertexAttribPointer(vColor, 3, gl.FLOAT, false, 0, 0);
  	gl.enableVertexAttribArray(vColor);
+
 
 
 	
