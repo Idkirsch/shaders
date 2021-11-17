1e6548bc647f062d67a19665f1fcff156e708184
Idkirsch <s195483@student.dtu.dk>
2021-11-17 11:53:18 +0100

    spinning 3d cube in perspective view, spinning around z axis

--- exercises_week1/Orbit/orbit.js
+++ exercises_week1/Orbit/orbit.js
@@ -1,19 +1,16 @@
 window.onload = function init(){
 
 
-	// var theta = [0, 0, 0];
-	// var xAxis = 0;
-	// var yAxis = 1;
-	// var zAxis = 2;
-	// var axis = 0;
+
 	var index = 0;
 	var canvas = document.getElementById("c");
 	var gl = canvas.getContext("webgl"); 
 	gl.clearColor(0.0, 0.0, 0.0, 0.9);
 	gl.clear(gl.COLOR_BUFFER_BIT); 
+	var currentAngle = [20.0, 20.0]; // x-akse, y-akse
+	initEventHandlers(canvas, currentAngle);
 
 	
-
 	var vertices = [
 		// X, Y , Z, ?
 		vec4(-0.5, -0.5, 0.5, 1.0),
@@ -27,7 +24,7 @@ window.onload = function init(){
 	];
 
 
-var indices = [
+	var indices = [
 		1, 0, 3,
 		3, 2, 1,
 		2, 3, 7,
@@ -41,7 +38,6 @@ var indices = [
 		5, 4, 0,
 		0, 1, 5
 	];
-
 
 
 
@@ -64,22 +60,67 @@ var indices = [
 	];
 
 	
+	function initEventHandlers(canvas, currentAngle){
+		var dragging = false;
+		var lastX = -1, lastY = -1;
 
+		canvas.onmousedown = function(ev){
+			var x = ev.clientX, y = ev.clientY;
+			var rect = ev.target.getBoundingClientRect();
+			if(rect.left <= x && x < rect.right && rect.top <= y && y< rect.bottom){
+				lastX = x; lastY = y;
+				dragging = true;
+			}
+		};
+
+		canvas.onmouseup = function(ev){
+			dragging = false;
+		}
+
+		canvas.onmousemove = function(ev){
+			var x = ev.clientX, y = ev.clientY;
+			if(dragging){
+				var factor = 100/canvas.height;
+				var dx = factor * (x - lastX);
+				var dy = factor * (y - lastY);
+				console.log("dx: "+dx+", dy: "+dy);
+
+				// Limit x-axis rotation angle to -90 to 90 degrees
+				currentAngle[0] = Math.max(Math.min(currentAngle[0]+dy, 90.0), -90.0);
+				currentAngle[1] = currentAngle[1]+dx;
+			}
+			lastX = x, lastY = y;
+		};
+	}
 
 
 
 	var program = initShaders(gl, "vertex-shader", "fragment-shader");
  	gl.useProgram(program);
  
- 	var eye = vec3(3,3,7);
+ 	var eye = vec3(3,3,2);
  	var up = vec3(0,1,0);
  	var at = vec3(0,0,0);
 
  	var View = lookAt(eye, at, up);
  	var Perspective = perspective(30, canvas.width/canvas.height, 0.1, 100);
+ 	
+ //this is the data that is required to rotate 
+	// var radian = Math.PI * currentAngle[0]/180.0; //converting to radians
+	// var cosB = Math.cos(radian), sinB = Math.sin(radian);
 
- 	var vLocation = gl.getUniformLocation(program, "modelViewMatrix");
- 	gl.uniformMatrix4fv(vLocation, false, flatten(View));
+	// var rotateMatrix = new Float32Array([
+	// 	cosB, sinB, .0, .0,
+	// 	-sinB, cosB, .0, .0,
+	// 	.0, .0, 1.0, .0,
+	// 	.0, .0, .0, 1.0
+	// 	]);
+
+	// var u_rotateMatrix = gl.getUniformLocation(program, "u_rotateMatrix");
+	// gl.uniformMatrix4fv(u_rotateMatrix, false, rotateMatrix);
+ 	
+ 	var u_modelViewMatrix = gl.getUniformLocation(program, "u_modelViewMatrix");
+ 	gl.uniformMatrix4fv(u_modelViewMatrix, false, flatten(View));
 
  	var u_projectionMatrix = gl.getUniformLocation(program, "u_projectionMatrix");
  	gl.uniformMatrix4fv(u_projectionMatrix, false, flatten(Perspective));
@@ -111,20 +152,37 @@ var indices = [
 	gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint8Array(indices),gl.STATIC_DRAW);
 
 
+	function tick(){
+		currentAngle[0] = currentAngle[0] + 0.1;
+		var radian = Math.PI * currentAngle[0]/180.0; //converting to radians
+		var cosB = Math.cos(radian), sinB = Math.sin(radian);
+
+		var rotateMatrix = new Float32Array([
+			cosB, sinB, .0, .0,
+			-sinB, cosB, .0, .0,
+			.0, .0, 1.0, .0,
+			.0, .0, .0, 1.0
+			]);
+		// the TA is pretty sure that I can use the library so I don't have to code
+		// all the different rotationmatrices by hand
+	
+
+		var u_rotateMatrix = gl.getUniformLocation(program, "u_rotateMatrix");
+		gl.uniformMatrix4fv(u_rotateMatrix, false, rotateMatrix);
 
 
-	render(gl, vertices.length, indices.length);
+		requestAnimationFrame(tick);
+		render(gl, vertices.length, indices.length);
+	}
+	tick();
 
 
 }
 
 
 function render(gl, numPoints, numVertices){
-
 	gl.clear(gl.COLOR_BUFFER_BIT);
-	// gl.drawArrays(gl.POINTS, 0, numPoints);
 	gl.drawElements(gl.TRIANGLES, numVertices, gl.UNSIGNED_BYTE, 0);
-
 }
 
 
